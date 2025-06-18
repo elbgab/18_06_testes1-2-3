ef1ce3e35b81db99dcbb32c3fc92f74854f8191b
elbgab <160151714+elbgab@users.noreply.github.com>
2025-06-18 10:30:28 -0300

    Add files via upload

---
+++ atividade1.py
@@ -0,0 +1,14 @@
+class Livro:
+    def __init__(self, titulo, autor, ano):
+        # Inicializa os atributos
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
+
+    def eh_antigo(self):
+   
+        return self.ano < 2000
+
+#teste
+livro1 = Livro("Dom Casmurro", "Machado de Assis", 1899)
+print(f"{livro1.titulo} é antigo?", livro1.eh_antigo())
---
+++ atividade2.py
@@ -0,0 +1,12 @@
+class Funcionario:
+    def __init__(self, nome, salario):
+        
+        self.nome = nome
+        self.salario = salario
+
+    def decimo_terceiro(self, meses_trabalhados):
+     
+        return (self.salario / 12) * meses_trabalhados
+#teste
+f = Funcionario("João", 3600)
+print("13º salário:", f.decimo_terceiro(8))  
